e5e0b67bce42bc1751af35977b0180ad60278d12
Yoshinari SOMAKI <5726167+sdmkun@users.noreply.github.com>
2024-08-16 12:23:06 +0900

    add: Next.jsでboardgame.io Typescript Demoが動くように

---
+++ src/app/boardgame/App.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+import { Client } from "boardgame.io/react";
+import { TicTacToe } from "./Game";
+import { Board } from "./Board";
+
+const App = Client({ game: TicTacToe, board: Board });
+
+export default App
